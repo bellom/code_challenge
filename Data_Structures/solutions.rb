55911fc4200f2b2e1991ad2f0dc9a3565ad7088e
bellom <bellomsean@gmail.com>
2019-04-28 08:55:21 +0100

    MIn Stack added

--- Data_Structures/solutions.rb
+++ Data_Structures/solutions.rb
@@ -460,8 +460,50 @@ puts queue.remove
 puts queue.list
 
 # ******************************
-# Linked LIst
+# Min Stack
 # ******************************
+class Stack
+
+  def initialize
+    @stack = []
+    @min = nil
+  end
+  
+  def push(number)
+    # your code here
+    @stack << number
+  end
+  
+  def pop
+    # your code here
+    @stack.pop
+  end
+  
+  def min
+    # your code here
+    @stack.min
+  end
+
+end
+
+stack = Stack.new
+stack.push(3)
+stack.push(5)
+puts stack.min
+# => 3
+
+stack.pop
+stack.push(7)
+puts stack.min
+# => 3
+
+stack.push(2)
+puts stack.min
+# => 2
+
+stack.pop
+puts stack.min
+# => 3
 
 # ******************************
 # Linked LIst
