973d447b2b0c021d27178ac312ba1ee9c536f300
bellom <bellomsean@gmail.com>
2019-04-28 08:42:42 +0100

    solution added

---
+++ Data_Structures/solutions.rb
@@ -0,0 +1,239 @@
+# ******************************
+# List
+# ******************************
+class Node
+    attr_accessor :value, :next_node
+    
+    def initialize(value, next_node = nil)
+        @value = value
+      @next_node = next_node
+    end
+  end
+  
+  class LinkedList
+    # setup head and tail
+    attr_accessor :head, :tail
+    
+    def initialize
+      @head = nil
+      @tail = nil
+    end
+    
+    def add(number)
+      # your code here
+      node = Node.new(number)
+      # Inserting first node
+      if @head == nil
+        @head = node
+        @tail = node
+      # Inserting rest nodes
+      else
+        @tail.next_node = node
+        @tail = node
+      end
+    end
+  
+    def get(index)
+      # your code here
+      count = 0
+      current = @head
+  
+      while !current.nil? do
+        if count == index
+          return current.value
+        else
+          count = count+1
+          current = current.next_node
+        end
+      end
+    end
+  end
+  
+  list = LinkedList.new
+  
+  list.add(3)
+  list.add(5)
+  puts list.get(1)
+  puts list.get(0)
+  
+# ******************************
+# Linked LIst
+# ******************************
+class Node
+    attr_accessor :value, :next_node
+    
+    def initialize(value, next_node = nil)
+        @value = value
+      @next_node = next_node
+    end
+  end
+  
+  class LinkedList
+    # setup head and tail
+    attr_accessor :head, :tail
+    
+    # INIT
+    def initialize
+      @head = nil
+      @tail = nil
+    end
+    
+    # ADD NODE AT END
+    def add(number)
+      # Create a new node
+      node = Node.new(number)
+      # Inserting first node
+      if @head == nil
+        @head = node
+        @tail = node
+      # Inserting rest nodes
+      else
+        @tail.next_node = node
+        @tail = node
+      end
+    end
+  
+    # ADD NODE AT INDEX
+    def add_at(index, item)
+  
+      node = Node.new(item)
+      if index == 0
+        node.next_node = @head
+        @head = node
+      else
+        prev_node = get_node(index-1)
+        current_node = get_node(index)
+          
+        prev_node.next_node = node
+        node.next_node = current_node
+      end
+    end
+    
+    # REMOVE NODE AT INDEX
+    def remove(index)
+      prev_node = get_node(index-1)
+      current_node = get_node(index)
+      
+      prev_node.next_node = current_node.next_node
+    end
+    
+    # FIND NODE AT INDEX
+    def get_node(index)
+      count = 0
+      current_node = @head
+      
+      while !current_node.nil? do
+        if count == index
+          return current_node
+        else
+          count = count + 1
+          current_node = current_node.next_node
+        end
+      end
+    end
+    
+    # FIND VALUE AT INDEX
+    def get(index)
+      count = 0
+      current_node = @head
+  
+      while !current_node.nil? do
+        if count == index
+          return current_node.value
+        else
+          count = count+1
+          current_node = current_node.next_node
+        end
+      end
+    end
+    
+    
+  end
+  
+  
+  
+  list = LinkedList.new
+  
+  list.add(3)
+  list.add(5)
+  list.add(7)
+  
+  list.add_at(1,10)
+  # list.get_node(0)
+  
+  list.head
+  
+  # p list.get(0)
+  # p list.get(1)
+
+  # ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
+
+# ******************************
+# Linked LIst
+# ******************************
