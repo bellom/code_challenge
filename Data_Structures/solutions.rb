af1a3a4d5761fabd29a0e93f6f93e32e0432b160
bellom <bellomsean@gmail.com>
2019-04-28 08:53:26 +0100

    stack solution added

--- Data_Structures/solutions.rb
+++ Data_Structures/solutions.rb
@@ -166,9 +166,150 @@ class Node
   # p list.get(0)
   # p list.get(1)
 
-  # ******************************
-# Linked LIst
 # ******************************
+# Stack
+# ******************************
+class Node
+  attr_accessor :value, :next_node
+  
+  def initialize(value, next_node = nil)
+	  @value = value
+    @next_node = next_node
+  end
+end
+
+class LinkedList
+  # setup head and tail
+  attr_accessor :head, :tail
+  
+  # INIT
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+  
+  # ADD NODE AT END
+  def add(number)
+    # Create a new node
+    node = Node.new(number)
+    # Inserting first node
+    if @head == nil
+      @head = node
+      @tail = node
+    # Inserting rest nodes
+    else
+      @tail.next_node = node
+      @tail = node
+    end
+  end
+
+  # ADD NODE AT INDEX
+  def add_at(index, item)
+
+    node = Node.new(item)
+    if index == 0
+      node.next_node = @head
+      @head = node
+    else
+      prev_node = get_node(index-1)
+      current_node = get_node(index)
+        
+      prev_node.next_node = node
+      node.next_node = current_node
+    end
+  end
+  
+  # REMOVE NODE AT INDEX
+  def remove(index)
+    prev_node = get_node(index-1)
+    current_node = get_node(index)
+    
+    prev_node.next_node = current_node.next_node
+  end
+  
+  # FIND NODE AT INDEX
+  def get_node(index)
+    count = 0
+    current_node = @head
+    
+    while !current_node.nil? do
+      if count == index
+        return current_node
+      else
+        count = count + 1
+        current_node = current_node.next_node
+      end
+    end
+  end
+  
+  # FIND VALUE AT INDEX
+  def get(index)
+    count = 0
+    current_node = @head
+
+    while !current_node.nil? do
+      if count == index
+        return current_node.value
+      else
+        count = count+1
+        current_node = current_node.next_node
+      end
+    end
+  end
+  
+  
+end
+
+
+
+list = LinkedList.new
+
+list.add(3)
+list.add(5)
+list.add(7)
+
+list.add_at(1,10)
+
+
+# Start with your code from LinkedList challenge.
+class Stack
+  def initialize
+    @head = nil
+  end
+
+  def push(value)
+    @head = Node.new(value, @head)
+  end
+
+  def pop
+    puts 'no values in the stack' if @head.nil?
+    value = @head.value
+    @head = @head.next_node
+    value
+  end
+
+end
+
+
+
+list = LinkedList.new
+
+stack = Stack.new
+stack.push(3)
+stack.push(5)
+puts stack.pop
+# => 5
+
+stack.push(2)
+stack.push(7)
+puts stack.pop
+# => 7
+
+puts stack.pop
+# => 2
+
+puts stack.pop
+# => 3
 
 # ******************************
 # Linked LIst
