ba6bed87d01cd5cf8adac031d3a3e213be6fed16
bellom <bellomsean@gmail.com>
2019-04-28 09:10:20 +0100

    trees added

--- Data_Structures/solutions.rb
+++ Data_Structures/solutions.rb
@@ -709,9 +709,20 @@ puts exact_sum?(11, [1, 5, 9, 13])
 # => false
 
 # ******************************
-# Linked LIst
+# Trees
 # ******************************
+def leftmost_nodes_sum(array)
+  # your code here
+  sum = 0; i = 0 
+  while i < array.length 
+    sum += array[i]
+    i = (2 * i + 1)
+  end
+    sum
+end
 
+puts leftmost_nodes_sum([2, 7, 5, 2, 6, 0, 9])
+# => 11
 # ******************************
 # Linked LIst
 # ******************************
