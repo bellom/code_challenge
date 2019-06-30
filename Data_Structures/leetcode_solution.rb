4ea09df57add035bb3fbec013b3007376e4366ad
bellom <bellomsean@gmail.com>
2019-06-30 09:12:47 +0100

    added Longest Palindromic Substring

--- Data_Structures/leetcode_solution.rb
+++ Data_Structures/leetcode_solution.rb
@@ -113,8 +113,26 @@ def median(combined_arr)
   end
 end
 # ******************************
-# 
+# Longest Palindromic Substring
 # ******************************
+def longest_palindrome(s)
+  r_s = s.reverse
+  c_lists = {}
+  index = 0
+  answer = ''
+  s.each_char do |c|
+      c_lists[c] ||= []
+      c_lists[c] << index
+      c_lists[c].each do |i|
+          if s[i..index] == r_s[(-index - 1)..(-i -1)] && answer.size < index - i + 1
+              answer = s[i..index]
+              break
+          end
+      end
+      index += 1
+  end
+  return answer
+end
 
 # ******************************
 # 
