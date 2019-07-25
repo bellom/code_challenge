9ccf63106de71e16ac6d66c151b9db9045f450c6
bellom <bellomsean@gmail.com>
2019-07-25 10:09:32 +0100

    added Climb Stairs

--- Data_Structures/leetcode_solution.rb
+++ Data_Structures/leetcode_solution.rb
@@ -543,9 +543,21 @@ def unique_paths(m, n)
 end
 
 # ******************************
-# 
+# Climb Stairs
 # ******************************
-
+var climbStairs = (n) => {
+    if(n <= 2) return n
+    let first  = 1
+    let second = 2
+    let result = 0
+    
+    for(let i = 3; i <= n ; i ++) {
+        result = first + second
+        first = second
+        second = result
+    }
+    return result
+}
 # ******************************
 # 
 # ******************************
