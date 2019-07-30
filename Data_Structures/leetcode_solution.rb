72311be976362347f2c963edac61cc0418ab9110
bellom <bellomsean@gmail.com>
2019-07-30 11:19:39 +0100

    addded minimum window substring

--- Data_Structures/leetcode_solution.rb
+++ Data_Structures/leetcode_solution.rb
@@ -710,9 +710,32 @@ var sortColors = function(nums) {
 };
 
 # ******************************
-# 
+# minimum window substring
 # ******************************
-
+function minWindow(s, t) {
+    var ans = '';
+    
+    var map = {};
+    t.split('').forEach(ch => map[ch] = (map[ch] || 0) + 1);
+    var count = Object.keys(map).length;
+    
+    var l = 0;
+    var r = -1;
+    
+    while (r < s.length) {
+        if (count === 0) {
+            if (!ans || r - l + 1 < ans.length) ans = s.slice(l, r + 1);
+            if (map[s[l]] !== undefined) map[s[l]]++;
+            if (map[s[l]] > 0) count++;
+            l++;
+        } else {
+            r++;
+            if (map[s[r]] !== undefined) map[s[r]]--;
+            if (map[s[r]] === 0) count--;
+        }
+    }
+    return ans;
+}
 # ******************************
 # 
 # ******************************
