b9a0d4bb7689dc308101dfa9a2bc1fb31b6b9f8c
bellom <bellomsean@gmail.com>
2019-07-31 10:17:58 +0100

    added subsets

--- Data_Structures/leetcode_solution.rb
+++ Data_Structures/leetcode_solution.rb
@@ -737,9 +737,19 @@ function minWindow(s, t) {
     return ans;
 }
 # ******************************
-# 
+# Subsets
 # ******************************
-
+var subsets = function(nums) {
+    let res = [[]]
+    
+    for(let i = 0; i < nums.length; i++){
+        let length = res.length;
+        for (let j = 0; j < length; j++){
+            res.push([...res[j], nums[i]]);
+        }
+    }
+    return res
+};
 # ******************************
 # 
 # ******************************
