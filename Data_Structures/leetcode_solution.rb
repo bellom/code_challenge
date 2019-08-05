cb0aaa40b408da65bdc6d978525d5fd8a81c21f9
bellom <bellomsean@gmail.com>
2019-08-05 04:18:21 +0100

    added word search

--- Data_Structures/leetcode_solution.rb
+++ Data_Structures/leetcode_solution.rb
@@ -751,8 +751,36 @@ var subsets = function(nums) {
     return res
 };
 # ******************************
-# 
+# word search 
 # ******************************
+def exist(board, word)
+  board.each_with_index do |row, i|
+    row.each_with_index do |cell, j|
+      return true if dfs(board, i, j, word)
+    end
+  end
+  false
+end
+
+def dfs(board, row, col, word)
+  return true if word.length == 0
+
+  if row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col] != word[0]
+    return false
+  end
+
+  temp = board[row][col]
+  board[row][col] = ''
+
+  result = dfs(board, row + 1, col, word[1..-1]) ||
+           dfs(board, row - 1, col, word[1..-1]) ||
+           dfs(board, row, col + 1, word[1..-1]) ||
+           dfs(board, row, col - 1, word[1..-1])
+
+  board[row][col] = temp
+
+  return result
+end
 
 # ******************************
 # 
