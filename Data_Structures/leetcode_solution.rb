76065a901c0a7ad3cd66bfb2d898980131bd609a
bellom <bellomsean@gmail.com>
2019-09-12 06:32:56 +0100

    added min stack

--- Data_Structures/leetcode_solution.rb
+++ Data_Structures/leetcode_solution.rb
@@ -1231,9 +1231,41 @@ var reverseList = function(head) {
     return prev;
 };
 # ******************************
-
+# Min Stack
 # ******************************
-# 
+var MinStack = function() {
+    this.arr = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    return this.arr.push(x);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    return this.arr.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.arr.slice(-1);
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return Math.min(...this.arr);
+};
+
 # ******************************
 
 # ******************************
